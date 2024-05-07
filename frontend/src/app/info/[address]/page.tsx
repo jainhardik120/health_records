7f245566110db242ea2e8db1435c873951cb0a26
Hardik Jain <jainhardik120@gmail.com>
2024-05-07 08:13:15 +0530

    Updates User Info Screen

--- frontend/src/app/info/[address]/page.tsx
+++ frontend/src/app/info/[address]/page.tsx
@@ -3,6 +3,76 @@
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify";
 
+
+interface DoctorProps {
+  details: {
+    address: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    specialization: string;
+    clinic_address: string;
+    clinic_contact: string;
+    type: number;
+  };
+}
+
+interface PatientProps {
+  details: {
+    address: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    dob: string;
+    gender: string;
+    paddress: string;
+    contact: string;
+    type: number;
+  };
+}
+
+const DoctorDetails: React.FC<DoctorProps> = ({ details }) => {
+  return (
+    <div>
+      <h2>Doctor Details</h2>
+      <ul>
+        <li><strong>Address:</strong> {details.address}</li>
+        <li><strong>Name:</strong> {details.first_name} {details.last_name}</li>
+        <li><strong>Email:</strong> {details.email}</li>
+        <li><strong>Specialization:</strong> {details.specialization}</li>
+        <li><strong>Clinic Address:</strong> {details.clinic_address}</li>
+        <li><strong>Clinic Contact:</strong> {details.clinic_contact}</li>
+      </ul>
+    </div>
+  );
+};
+
+const PatientDetails: React.FC<PatientProps> = ({ details }) => {
+  return (
+    <div>
+      <h2>Patient Details</h2>
+      <ul>
+        <li><strong>Address:</strong> {details.address}</li>
+        <li><strong>Name:</strong> {details.first_name} {details.last_name}</li>
+        <li><strong>Email:</strong> {details.email}</li>
+        <li><strong>Date of Birth:</strong> {details.dob}</li>
+        <li><strong>Gender:</strong> {details.gender}</li>
+        <li><strong>Permanent Address:</strong> {details.paddress}</li>
+        <li><strong>Contact:</strong> {details.contact}</li>
+      </ul>
+    </div>
+  );
+};
+
+const UserDetails: React.FC<{ details: any }> = ({ details }) => {
+  return (
+    <div>
+      {details.type === 1 && <DoctorDetails details={details} />}
+      {details.type === 2 && <PatientDetails details={details} />}
+    </div>
+  );
+};
+
 export default function Page({ params }: { params: { address: string } }) {
   const [details, setdetails] = useState({});
   useEffect(() => {
@@ -25,13 +95,7 @@ export default function Page({ params }: { params: { address: string } }) {
   return (
     <>
       <div className="flex w-full justify-center mt-4">
-        <ul>
-          {Object.entries(details).map(([key, value]: [string, any]) => (
-            <li key={key}>
-              <strong>{key}: </strong> {value}
-            </li>
-          ))}
-        </ul>
+        <UserDetails details={details}/>
       </div>
     </>
   )
